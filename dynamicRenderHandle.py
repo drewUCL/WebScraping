d93959cd8f655531c5f8611137d1a1a16f9dbb62
Andrew Mann <Andrew Mann>
2016-06-05 21:54:06 +0100

    fixed scraping focus as element moves when hitting the page each time

--- dynamicRenderHandle.py
+++ dynamicRenderHandle.py
@@ -12,6 +12,11 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from selenium.common.exceptions import ElementNotVisibleException
+from selenium.webdriver.common.action_chains import ActionChains
+
+from selenium.webdriver.common.keys import Keys
+
+from bs4 import BeautifulSoup
 
 
 def init_driver():
@@ -19,23 +24,45 @@ def init_driver():
 	driver.wait = WebDriverWait(driver, 5)
 	return driver
 	
+def filterDirtyData(browser):
+	html_source = browser.page_source
+	soup = BeautifulSoup(html_source,'html.parser') 
+	scrape_data = soup.findAll('table',{'class':'genTbl openTbl ecHistoryTbl'})
+	print scrape_data
+	for row in scrape_data:
+		cells = row.find_all("td")
+		print cells 
+		#rn = cells.get_text() #cells[0]
+		#print rn
+	
 def showMore(browser,URL,XPATH):
 	browser.get(URL)
-	count=0
+	time.sleep(15) # let page load
+	count = 0
 	for a in browser.find_elements_by_xpath(XPATH):
 		link_text = a.text
-		if("Show more" in link_text):
-			while(count < 5):
-				try:
-					print "[info] Button to click: %s" % a.text
-					print "[info] DOM sleep: 15 seconds"
-					time.sleep(15)
-					a.click()
-				except:
-					print "[info] Reconfig click number and try again"
-					# handle
-					pass
-				count += 1
+		#if("Show more" in link_text):
+		while(count < 2):
+			try:
+				print "[info] Button to click: %s" % a.text
+				print "[info] DOM sleep: 15 seconds"
+				time.sleep(15)
+				#browser.execute_script("document.getElementById('id').scrollIntoView();")
+				#browser.find_element_by_id("eventHistoryTable345").send_keys(Keys.NULL)
+				browser.execute_script("window.scrollBy(0, -150);")
+				action = webdriver.common.action_chains.ActionChains(browser)
+				action.move_to_element(a)
+				action.click()
+				action.perform()
+				print "[Massage] Success"
+			except:
+				print "[Message] Could not find element location\n"
+				# handle - try to re focus as the element moves down the page
+				browser.execute_script("window.scrollBy(0, -150);")
+			count += 1
+			
+	# Now scrape the page contents
+	filterDirtyData(browser)
 
 
 if __name__ == "__main__":
